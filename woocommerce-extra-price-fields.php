0f10056218cb5985b4aeef557c790882c133bedc
aman086 <aman086@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-18 12:59:58 +0000

    updated to 1.1
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-extra-price-fields/trunk@860146 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- woocommerce-extra-price-fields.php
+++ woocommerce-extra-price-fields.php
@@ -28,59 +28,52 @@
 
 
 
-function add_custom_price_box() {
+  function add_custom_price_box() {
 
-    woocommerce_wp_text_input(array('id' => '_per100gm_price', 'class' => 'wc_input_per100gmprice short', 'label' => __('Per 100g Price', 'woocommerce') . ' (' . get_woocommerce_currency_symbol() . ')', 'type' => 'number', 'custom_attributes' => array(
-            'step' => 'any',
-            'min' => '0'
-            )));
-
-    woocommerce_wp_text_input(array('id' => '_per100_pieces', 'class' => 'wc_input__per100_pieces short', 'label' => __('Per 100 Pieces', 'woocommerce') . ' (' . get_woocommerce_currency_symbol() . ')', 'type' => 'number', 'custom_attributes' => array(
-            'step' => 'any',
-            'min' => '0'
-            )));
-}
-
-add_action('woocommerce_product_options_pricing', 'add_custom_price_box');
-
-function custom_woocommerce_process_product_meta($post_id, $post) {
-
-    update_post_meta($post_id, '_per100gm_price', stripslashes($_POST['_per100gm_price']));
-
-    update_post_meta($post_id, '_per100_pieces', stripslashes($_POST['_per100_pieces']));
-}
-
-add_action('woocommerce_process_product_meta', 'custom_woocommerce_process_product_meta', 2, 2);
-
-function add_custom_price_front($p, $obj) {
-
-    $post_id = $obj->post->ID;
+  	woocommerce_wp_text_input(array('id' => 'pro_price_extra_info', 'class' => 'wc_input_price_extra_info short', 'label' => __('Price Extra Info', 'woocommerce') . ' (' . get_woocommerce_currency_symbol() . ')', 'type' => 'number', 'custom_attributes' => array(
+  	                          'step' => 'any',
+  	                          'min' => '0'
+  	                          )));
 
 
-    $link = get_permalink($post_id);
-    $per100gm = get_post_meta($post_id, '_per100gm_price', true);
+  }
 
-    $per100piece = get_post_meta($post_id, '_per100_pieces', true);
+  add_action('woocommerce_product_options_pricing', 'add_custom_price_box');
 
-    if (is_admin()) {
-        $tag = 'div'; //show in new line
-    } else {
-        $tag = 'span';
-    }
+  function custom_woocommerce_process_product_meta($post_id, $post) {
 
-    if (!empty($per100gm)) {
-        $additional_price.= "<$tag style='font-size:80%' class='price_per100gm'> (" . get_woocommerce_currency_symbol() . "100g=$per100gm)</$tag>";
-    }
-    if (!empty($per100piece)) {
-        $additional_price.= "<$tag style='font-size:80%' class='price_per100piece'> (" . get_woocommerce_currency_symbol() . "100 Pieces=$per100piece)</$tag>";
-    }
+  	update_post_meta($post_id, 'pro_price_extra_info', stripslashes($_POST['pro_price_extra_info']));
+  }
+
+  add_action('woocommerce_process_product_meta', 'custom_woocommerce_process_product_meta', 2, 2);
+
+  function add_custom_price_front($p, $obj) {
+
+  	$post_id = $obj->post->ID;
+
+  	$pro_price_extra_info = get_post_meta($post_id, 'pro_price_extra_info', true);
+
+  	if (is_admin()) {
+  			//show in new line
+  		$tag = 'div';
+  	} else {
+  		$tag = 'span';
+  	}
+
+  	if (!empty($pro_price_extra_info)) {
+  		$additional_price= "<$tag style='font-size:80%' class='pro_price_extra_info'> $pro_price_extra_info</$tag>";
+  	}
 
 
-    return "<a href='$link'>" . $p . $additional_price . "</a>";
-}
+  	if( !empty($additional_price)	 )
+  		return  $p . $additional_price ;
+  	else
+  		return  $p ;
 
-add_filter('woocommerce_get_price_html', 'add_custom_price_front', 10, 2);
-add_filter('woocommerce_get_price_html', 'add_custom_price_front', 10, 2);
 
 
-?>
+  }
+
+  add_filter('woocommerce_get_price_html', 'add_custom_price_front', 10, 2);
+
+  ?>
